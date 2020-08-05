38a2ff06f2144bec1158581e10d7c16ef3f1b0c9
joxpg <joxpg@hotmail.com>
2020-08-04 22:07:10 -0500

    Add files via upload

---
+++ hello-world/SpringBootApplicationLuncher.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class SpringBootApplicationLuncher {
+
+public static void main(String[] args) {
+
+SpringApplication.run(HelloWorldController.class, args);
+
+}
+
+}
